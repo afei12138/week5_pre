a6565cd014d7b2f3820cc0950d0835c8576f8bb5
afei12138 <30309796@qq.com>
2022-04-24 06:23:40 +0000

    change to red

--- heart.c
+++ heart.c
@@ -4,7 +4,7 @@ int main(int argc, const char * argv[]) {
     for (y = 1.5f; y > -1.5f; y -= 0.1f) {
         for (x = -1.5f; x < 1.5; x += 0.05f) {
             a = x * x + y * y - 1;
-	    //printf("\033[31m");
+	    printf("\033[31m");
             putchar(a * a * a - x * x * y * y * y <= 0.0f ? '#' : ' ');
         }
         putchar('\n');
