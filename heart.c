ecc18f4813854aa17f75a0aea34729a412cc3cbf
afei12138 <30309796@qq.com>
2022-04-24 06:26:03 +0000

    change to o

--- heart.c
+++ heart.c
@@ -5,7 +5,7 @@ int main(int argc, const char * argv[]) {
         for (x = -1.5f; x < 1.5; x += 0.05f) {
             a = x * x + y * y - 1;
 	    //printf("\033[31m");
-            putchar(a * a * a - x * x * y * y * y <= 0.0f ? '#' : ' ');
+            putchar(a * a * a - x * x * y * y * y <= 0.0f ? 'o' : ' ');
         }
         putchar('\n');
     }
