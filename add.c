19e50521543d2ac385adad5df8350412440337cc
afei12138 <30309796@qq.com>
2022-04-24 06:17:48 +0000

    correct code

--- add.c
+++ add.c
@@ -1,8 +1,8 @@
 #include "add.h"
 void add(){
         //测试非法内存，产生core文件
-	int *ptr = NULL;
-        *ptr = 10;
+	//int *ptr = NULL;
+        // *ptr = 10;
         //打印土味情话
         printf("      		     你会喜欢我吗？\n");
 	printf("        	     不会我教你啊。\n");
